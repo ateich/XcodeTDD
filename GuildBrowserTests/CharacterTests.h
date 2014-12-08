5a00be15f74b9b1f9b5911d2df6218676e890362
Andrew Teich <andrewteich@me.com>
2014-12-08 14:25:31 -0800

    Created local data test stubs.

---
+++ GuildBrowserTests/CharacterTests.h
@@ -0,0 +1,18 @@
+//
+//  CharacterTests.h
+//  GuildBrowser
+//
+//  Created by HackReactor on 12/8/14.
+//  Copyright (c) 2014 Charlie Fulton. All rights reserved.
+//
+#import <SenTestingKit/SenTestingKit.h>
+#import "WoWUtils.h"
+#import "Character.h"
+
+@interface CharacterTests : SenTestCase{
+    
+}
+
+
+
+@end
